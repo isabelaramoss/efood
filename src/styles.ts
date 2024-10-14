ed43a9730947fb6dba3108f942d1f8671d03b5a2
Isabela Ramos <isamrteixeira@gmail.com>
2024-10-14 00:43:20 -0300

    :iphone: breakpoints

--- src/styles.ts
+++ src/styles.ts
@@ -6,3 +6,33 @@ export const colors = {
   beige: '#FFF8F2',
   white: '#FFF'
 }
+
+export const breakpoints = {
+  desktop: '1024px',
+  mobile: '768px'
+}
+
+export const GlobalStyle = createGlobalStyle`
+  *{
+    margin: 0;
+    padding: 0;
+    box-sizing: border-box;
+    font-family: Roboto, sans-serif;
+    list-style: none;
+  }
+
+  body{
+    background-color: ${colors.beige};
+    color: ${colors.primary};
+  }
+
+  .container{
+    max-width: 1024px;
+    width: 100%;
+    margin: 0 auto;
+
+    @media (max-width: ${breakpoints.desktop}){
+      max-width: 80%
+    }
+  }
+`
