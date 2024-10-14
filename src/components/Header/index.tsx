9689a34bfbbb0b46de5d74ee2fb386661d2f26fc
Isabela Ramos <isamrteixeira@gmail.com>
2024-10-14 11:13:00 -0300

    feat: adicão de componente Header

---
+++ src/components/Header/index.tsx
@@ -0,0 +1,10 @@
+const Header = () => (
+  <div className="headerbar">
+    <div>
+      <img src="" alt="" />
+      <h1>Viva experiências gastrônomicas no conforto da sua casa</h1>
+    </div>
+  </div>
+)
+
+export default Header
