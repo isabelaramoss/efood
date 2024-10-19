174e1876ad4a32d9da6edc3a43bd0adcecd01f12
Isabela Ramos <isamrteixeira@gmail.com>
2024-10-19 16:17:48 -0300

    feat: adição de Banner à página FoodMenu.

--- src/pages/FoodMenu/index.tsx
+++ src/pages/FoodMenu/index.tsx
@@ -1,8 +1,10 @@
+import Banner from '../../components/Banner'
 import HeaderFoodMenu from '../../components/HeaderFoodMenu'
 
 const FoodMenu = () => (
   <>
     <HeaderFoodMenu />
+    <Banner />
   </>
 )
 
