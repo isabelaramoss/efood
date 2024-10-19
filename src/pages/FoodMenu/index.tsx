8bd8b2d1c362cb83cbc8e9a7890abf2de81f9143
Isabela Ramos <isamrteixeira@gmail.com>
2024-10-19 18:05:48 -0300

    feat: adição de array com itens à página MenuFood

--- src/pages/FoodMenu/index.tsx
+++ src/pages/FoodMenu/index.tsx
@@ -1,12 +1,59 @@
+import MenuFood from '../../models/MenuFood'
 import Banner from '../../components/Banner'
-import MenuCard from '../../components/FoodMenuCard'
 import HeaderFoodMenu from '../../components/HeaderFoodMenu'
+import pizza from '../../assets/pizza.png'
+import MenuList from '../../components/FoodMenuList'
+
+const menuCard: MenuFood[] = [
+  {
+    image: pizza,
+    title: 'Pizza Marguerita',
+    description:
+      'A clássica Marguerita: molho de tomate suculento, mussarela derretida, manjericão fresco e um toque de azeite. Sabor e simplicidade!',
+    id: 1
+  },
+  {
+    image: pizza,
+    title: 'Pizza Marguerita',
+    description:
+      'A clássica Marguerita: molho de tomate suculento, mussarela derretida, manjericão fresco e um toque de azeite. Sabor e simplicidade!',
+    id: 2
+  },
+  {
+    image: pizza,
+    title: 'Pizza Marguerita',
+    description:
+      'A clássica Marguerita: molho de tomate suculento, mussarela derretida, manjericão fresco e um toque de azeite. Sabor e simplicidade!',
+    id: 3
+  },
+  {
+    image: pizza,
+    title: 'Pizza Marguerita',
+    description:
+      'A clássica Marguerita: molho de tomate suculento, mussarela derretida, manjericão fresco e um toque de azeite. Sabor e simplicidade!',
+    id: 4
+  },
+  {
+    image: pizza,
+    title: 'Pizza Marguerita',
+    description:
+      'A clássica Marguerita: molho de tomate suculento, mussarela derretida, manjericão fresco e um toque de azeite. Sabor e simplicidade!',
+    id: 5
+  },
+  {
+    image: pizza,
+    title: 'Pizza Marguerita',
+    description:
+      'A clássica Marguerita: molho de tomate suculento, mussarela derretida, manjericão fresco e um toque de azeite. Sabor e simplicidade!',
+    id: 6
+  }
+]
 
 const FoodMenu = () => (
   <>
     <HeaderFoodMenu />
     <Banner />
-    <MenuCard />
+    <MenuList items={menuCard} />
   </>
 )
 
