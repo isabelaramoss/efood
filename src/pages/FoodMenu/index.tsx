14ae2c59d97203af703b3b67ee6be1052b550fa0
Isabela Ramos <isamrteixeira@gmail.com>
2024-10-19 16:22:32 -0300

    feat: adição de MenuCard à página FoodMenu.

--- src/pages/FoodMenu/index.tsx
+++ src/pages/FoodMenu/index.tsx
@@ -1,10 +1,12 @@
 import Banner from '../../components/Banner'
+import MenuCard from '../../components/FoodMenuCard'
 import HeaderFoodMenu from '../../components/HeaderFoodMenu'
 
 const FoodMenu = () => (
   <>
     <HeaderFoodMenu />
     <Banner />
+    <MenuCard />
   </>
 )
 
