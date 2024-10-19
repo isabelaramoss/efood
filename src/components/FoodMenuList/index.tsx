bb6a2953ae051b0f0d83438c71a04ca244c48780
Isabela Ramos <isamrteixeira@gmail.com>
2024-10-19 16:21:53 -0300

    feat: adição e estilização de componente FoodMenuList.

---
+++ src/components/FoodMenuList/index.tsx
@@ -0,0 +1,9 @@
+const MenuList = () => (
+  <div className="food-container container">
+    <li>
+      <img src="" alt="" />
+    </li>
+  </div>
+)
+
+export default MenuList
---
+++ src/components/FoodMenuList/style.ts
@@ -0,0 +1,5 @@
+import styled from 'styled-components'
+
+export const x = styled.div`
+  margin-bottom: 3px;
+`
