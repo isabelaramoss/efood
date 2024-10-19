821f0ab1beb4c5b3e2b5bbc125c6345bbe2511fd
Isabela Ramos <isamrteixeira@gmail.com>
2024-10-19 18:04:05 -0300

    feat: adição de Props.

--- src/components/FoodMenuCard/index.tsx
+++ src/components/FoodMenuCard/index.tsx
@@ -1,33 +1,18 @@
 import { ButtonAdd, Descr, FoodContainer, InfoContent, Title } from './style'
-import img from '../../assets/pizza.png'
 
-// type Props = {
-//   image: string
-//   title: string
-//   description: string
-//   id: number
-// }
+type Props = {
+  image: string
+  title: string
+  description: string
+  id: number
+}
 
-// const MenuCard = ({ image, title, description }: Props) => (
-//   <FoodContainer>
-//     <img src={image} alt="" />
-//     <div className="infoContent">
-//       <h4>{title}</h4>
-//       <p className="descr">{description}</p>
-//     </div>
-//     <button>Adicionar ao carrinho</button>
-//   </FoodContainer>
-// )
-
-const MenuCard = () => (
+const MenuCard = ({ image, title, description }: Props) => (
   <FoodContainer>
-    <img src={img} alt="" />
+    <img src={image} alt="" />
     <InfoContent>
-      <Title>Pizza Marguerita</Title>
-      <Descr className="descr">
-        A clássica Marguerita: molho de tomate suculento, mussarela derretida,
-        manjericão fresco e um toque de azeite. Sabor e simplicidade!
-      </Descr>
+      <Title>{title}</Title>
+      <Descr>{description}</Descr>
     </InfoContent>
     <ButtonAdd>Adicionar ao carrinho</ButtonAdd>
   </FoodContainer>
