d60bcd7af026cd16cad52863bc09bb7127a61cda
Isabela Ramos <isamrteixeira@gmail.com>
2024-10-19 18:04:28 -0300

    feat: adição de Props e estilização do componente.

--- src/components/FoodMenuList/index.tsx
+++ src/components/FoodMenuList/index.tsx
@@ -1,9 +1,27 @@
-const MenuList = () => (
-  <div className="food-container container">
-    <li>
-      <img src="" alt="" />
-    </li>
-  </div>
+import MenuFood from '../../models/MenuFood'
+import MenuCard from '../FoodMenuCard'
+import { List, MenuListContainer } from './style'
+
+export type Props = {
+  items: MenuFood[]
+}
+
+const MenuList = ({ items }: Props) => (
+  <MenuListContainer>
+    <div className="container">
+      <List>
+        {items.map((food) => (
+          <MenuCard
+            key={food.id}
+            image={food.image}
+            title={food.title}
+            description={food.description}
+            id={food.id}
+          />
+        ))}
+      </List>
+    </div>
+  </MenuListContainer>
 )
 
 export default MenuList
--- src/components/FoodMenuList/style.ts
+++ src/components/FoodMenuList/style.ts
@@ -1,5 +1,20 @@
 import styled from 'styled-components'
+import { Props } from '.'
+import { breakpoints } from '../../styles'
 
-export const x = styled.div`
-  margin-bottom: 3px;
+export const MenuListContainer = styled.section<Omit<Props, 'items'>>`
+  padding: 32px o;
+`
+
+export const List = styled.ul`
+  display: grid;
+  grid-template-columns: repeat(3, 1fr);
+  column-gap: 32px;
+  row-gap: 32px;
+
+  @media (max-width: ${breakpoints.desktop}) {
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+  }
 `
